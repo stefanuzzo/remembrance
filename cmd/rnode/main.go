fb8f6f9ce1432bf0be839f7496de651dd761d4ad
stefanuzzo <stefano.altavilla@gmail.com>
2023-11-02 04:28:40 +0100

    Added reflective support for configuration.

--- cmd/rnode/main.go
+++ cmd/rnode/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"reflect"
-	"strings"
 
 	"github.com/stefanuzzo/internal/configuration"
 )
@@ -18,16 +16,6 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
-	}
-
-	t := reflect.TypeOf(c)
-	fmt.Println(t)
-
-	nFields := t.NumField()
-	fmt.Printf("fields: %d\n", nFields)
-	for i := 0; i < nFields; i++ {
-		f := t.Field((i))
-		fmt.Println(strings.ToLower(f.Name))
 	}
 
 	fmt.Printf("mode: %s\n", c.Mode)
--- internal/pkg/configuration/configuration.go
+++ internal/pkg/configuration/configuration.go
@@ -4,7 +4,11 @@ package configuration
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
+	"reflect"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -14,13 +18,11 @@ type Configuration struct {
 
 const defaultConfigurationFilePath = "./config.json"
 
-// TODO make a pointer and protect with a mutex
 var (
 	currentConfiguration *Configuration
-
-	loaded bool = false
-
-	guard sync.RWMutex
+	loaded               bool = false
+	guard                sync.RWMutex
+	flagsMap             map[string]*string
 )
 
 func GetConfiguration(forceReload bool, configurationFilePath string) (Configuration, error) {
@@ -46,14 +48,113 @@ func GetConfiguration(forceReload bool, configurationFilePath string) (Configura
 	return *currentConfiguration, nil
 }
 
-func applyEnvironmentVariablesValues(configuration *Configuration) {
-	mode, present := os.LookupEnv("REMEMBRANCE_MODE")
+func applyEnvironmentVariableValue(field *reflect.StructField, value *reflect.Value) error {
+	variableName, present := field.Tag.Lookup("env")
+	if !present {
+		variableName = "REMEMBRANCE_" + strings.ToUpper(field.Name)
+	}
+
+	variableValue, present := os.LookupEnv(variableName)
 	if present {
-		configuration.Mode = mode
+		fieldValue := value.Elem().FieldByName(field.Name)
+
+		actualValue, err := getValue(field, variableValue)
+		if err != nil {
+			return err
+		}
+
+		fieldValue.Set(*actualValue)
+	}
+
+	return nil
+}
+
+func applyConfigurationFileValue(field *reflect.StructField, value *reflect.Value, jsonConfiguration map[string]interface{}) error {
+
+	attributeName, present := field.Tag.Lookup("json")
+	if !present {
+		attributeName = strings.ToLower(field.Name)
+	}
+
+	fieldValue := value.Elem().FieldByName(field.Name)
+
+	attributeValue, present := jsonConfiguration[attributeName]
+	if present && attributeValue != nil {
+		switch attributeType := attributeValue.(type) {
+		case string:
+			if attributeType != "" {
+				fieldValue.Set(reflect.ValueOf(attributeType))
+			}
+
+		default:
+			s := fmt.Sprint(attributeValue)
+			actualValue, err := getValue(field, s)
+			if err != nil {
+				return err
+			}
+
+			fieldValue.Set(*actualValue)
+		}
+	}
+
+	return nil
+}
+
+func prepareFlags(pt *reflect.Type) {
+	const blankValue = "blank"
+
+	flagsMap = make(map[string]*string)
+
+	t := *pt
+	nFields := t.NumField()
+	for i := 0; i < nFields; i++ {
+		field := t.Field(i)
+
+		argumentName, present := field.Tag.Lookup("cmd")
+		if !present {
+			argumentName = strings.ToLower(field.Name)
+		}
+
+		usage := fmt.Sprintf("Usage: %s", argumentName)
+		argumentValue := flag.String(argumentName, blankValue, usage)
+		flagsMap[argumentName] = argumentValue
+	}
+
+	if !flag.Parsed() {
+		flag.Parse()
 	}
 }
 
-func applyConfigurationFileValues(configuration *Configuration, configurationFilePath string) error {
+func applyCommandLineValue(field *reflect.StructField, value *reflect.Value) error {
+	const blankValue = "blank"
+
+	argumentName, present := field.Tag.Lookup("cmd")
+	if !present {
+		argumentName = strings.ToLower(field.Name)
+	}
+
+	argumentValue := flagsMap[argumentName]
+
+	if (*argumentValue) != blankValue {
+		actualValue, err := getValue(field, *argumentValue)
+		if err != nil {
+			return err
+		}
+
+		fieldValue := value.Elem().FieldByName(field.Name)
+		fieldValue.Set(*actualValue)
+	}
+
+	return nil
+}
+
+func applyDefaultValues(configuration *Configuration) {
+	configuration.Mode = "default"
+}
+
+func loadConfiguration(configuration *Configuration, configurationFilePath string) error {
+	applyDefaultValues(configuration)
+
 	var actualConfigurationFilePath string
 	if configurationFilePath == "" {
 		actualConfigurationFilePath = defaultConfigurationFilePath
@@ -75,48 +176,154 @@ func applyConfigurationFileValues(configuration *Configuration, configurationFil
 
 	jsonConfiguration := rawConfiguration.(map[string]interface{})
 
-	mode, found := jsonConfiguration["mode"]
-	if found {
-		switch mode := mode.(type) {
-		case string:
-			if mode != "" {
-				configuration.Mode = mode
-			}
+	t := reflect.TypeOf(*configuration)
+	v := reflect.ValueOf(configuration)
+
+	prepareFlags(&t)
+
+	nFields := t.NumField()
+	for i := 0; i < nFields; i++ {
+		field := t.Field(i)
+
+		if err := applyConfigurationFileValue(&field, &v, jsonConfiguration); err != nil {
+			return fmt.Errorf("error while applying value to field '%s': %s", field.Name, err.Error())
+		}
+
+		if err := applyEnvironmentVariableValue(&field, &v); err != nil {
+			return fmt.Errorf("error while applying value to field '%s': %s", field.Name, err.Error())
+		}
+
+		if err := applyCommandLineValue(&field, &v); err != nil {
+			return fmt.Errorf("error while applying value to field '%s': %s", field.Name, err.Error())
 		}
 	}
 
 	return nil
 }
 
-func applyDefaultValues(configuration *Configuration) {
-	configuration.Mode = "default"
-}
+func getValue(field *reflect.StructField, value string) (*reflect.Value, error) {
+	switch field.Type.Kind() {
+	case reflect.Float32:
+		actual, err := strconv.ParseFloat(value, 32)
+		if err != nil {
+			return nil, err
+		}
 
-func applyCommandLineValues(configuration *Configuration) {
-	const blankModeValue = "blank"
+		result := reflect.ValueOf(float32(actual))
+		return &result, nil
 
-	mode := flag.String("mode", blankModeValue, "The mode this node has to be started")
+	case reflect.Float64:
+		actual, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			return nil, err
+		}
 
-	if !flag.Parsed() {
-		flag.Parse()
+		result := reflect.ValueOf(actual)
+		return &result, nil
+
+	case reflect.Int:
+		actual, err := strconv.ParseInt(value, 0, 0)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(int(actual))
+		return &result, nil
+
+	case reflect.Int8:
+		actual, err := strconv.ParseInt(value, 0, 8)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(int8(actual))
+		return &result, nil
+
+	case reflect.Int16:
+		actual, err := strconv.ParseInt(value, 0, 16)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(int16(actual))
+		return &result, nil
+
+	case reflect.Int32:
+		actual, err := strconv.ParseInt(value, 0, 32)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(int32(actual))
+		return &result, nil
+
+	case reflect.Int64:
+		actual, err := strconv.ParseInt(value, 0, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(int64(actual))
+		return &result, nil
+
+	case reflect.Uint:
+		actual, err := strconv.ParseUint(value, 0, 0)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(uint(actual))
+		return &result, nil
+
+	case reflect.Uint8:
+		actual, err := strconv.ParseUint(value, 0, 8)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(uint8(actual))
+		return &result, nil
+
+	case reflect.Uint16:
+		actual, err := strconv.ParseUint(value, 0, 16)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(uint16(actual))
+		return &result, nil
+
+	case reflect.Uint32:
+		actual, err := strconv.ParseUint(value, 0, 32)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(uint32(actual))
+		return &result, nil
+
+	case reflect.Uint64:
+		actual, err := strconv.ParseUint(value, 0, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(uint64(actual))
+		return &result, nil
+
+	case reflect.Bool:
+		actual, err := strconv.ParseBool(value)
+		if err != nil {
+			return nil, err
+		}
+
+		result := reflect.ValueOf(actual)
+		return &result, nil
+
+	case reflect.String:
+		result := reflect.ValueOf(value)
+		return &result, nil
 	}
 
-	if (*mode) != blankModeValue {
-		configuration.Mode = *mode
-	}
-}
-
-func loadConfiguration(configuration *Configuration, configurationFilePath string) error {
-	applyDefaultValues(configuration)
-
-	err := applyConfigurationFileValues(configuration, configurationFilePath)
-	if err != nil {
-		return err
-	}
-
-	applyEnvironmentVariablesValues(configuration)
-
-	applyCommandLineValues(configuration)
-
-	return nil
+	return nil, fmt.Errorf("unexpected field type: %s", field.Type.String())
 }
